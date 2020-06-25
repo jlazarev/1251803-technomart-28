11fd49110e108b55076e987316c97488d3a906f9
jlazarev <lazarev.haven@gmail.com>
2020-06-25 17:03:31 +0300

    добавил скрипт по нажатию на закладки

--- js/basket.js
+++ js/basket.js
@@ -1,15 +1,27 @@
-let basketLink = document.querySelector(".basket-link");
+let basketLinks = document.querySelectorAll(".basket-link");
 let basketPopup = document.querySelector(".basket-popup");
 let basketClose = basketPopup.querySelector(".basket-close");
+let basketProceed = basketPopup.querySelector(".basket-popup-button");
+let cart = document.querySelector(".cart");
 
-basketLink.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	basketPopup.classList.add("basket-popup-open");
-});
+for (var i = 0; i < basketLinks.length; i++) {
+  basketLinks[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    basketPopup.classList.add("basket-popup-open");
+    if (!cart.classList.contains("cart-full")) {
+      cart.classList.add("cart-full");
+    }
+  });  
+}
 
 basketClose.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	basketPopup.classList.remove("basket-popup-open");
+});
+
+basketProceed.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  basketPopup.classList.remove("basket-popup-open");
 });
 
 window.addEventListener("keydown", function (evt) {
