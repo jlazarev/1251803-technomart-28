d33d50a3330e942faea65098f94035c148c6702b
Evgeniy Lazarev <lazarev.haven@gmail.com>
2020-07-06 22:32:11 +0300

    работа над ошибками

--- js/script.js
+++ js/script.js
@@ -60,17 +60,17 @@ window.addEventListener("keydown", function (evt) {
 
 // slider
 
-var radioButtons = document.querySelectorAll('.radio-button');
-var sliderItems = document.querySelectorAll('.slider-item');
+var radioButtons = document.querySelectorAll(".radio-button");
+var sliderItems = document.querySelectorAll(".slider-item");
 
-var back = document.querySelector('.back');
-var forward = document.querySelector('.forward');
+var back = document.querySelector(".back");
+var forward = document.querySelector(".forward");
 
 var addThumbnailClickHandler = function (item, button) {
-    button.addEventListener('click', function () {
+    button.addEventListener("click", function () {
       for (var j = 0; j < sliderItems.length; j++) {
-        radioButtons[j].classList.remove('radio-button-checked');
-        sliderItems[j].classList.remove('slider-show');
+        radioButtons[j].classList.remove("radio-button-checked");
+        sliderItems[j].classList.remove("slider-show");
       }
 
       if (button === radioButtons[0]) {
@@ -85,8 +85,8 @@ var addThumbnailClickHandler = function (item, button) {
         forward.removeAttribute("disabled");
       }
 
-      button.classList.add('radio-button-checked');
-      item.classList.add('slider-show');
+      button.classList.add("radio-button-checked");
+      item.classList.add("slider-show");
     });
 };
 
@@ -94,15 +94,15 @@ for (var i = 0; i < sliderItems.length; i++) {
     addThumbnailClickHandler(sliderItems[i], radioButtons[i]);
 }
 
-forward.addEventListener('click', function () {
+forward.addEventListener("click", function () {
   for (var j = 0; j < sliderItems.length; j++) {
-      if (sliderItems[j].classList.contains('slider-show')) {
+      if (sliderItems[j].classList.contains("slider-show")) {
         
-        sliderItems[j].classList.remove('slider-show');
-        sliderItems[j + 1].classList.add('slider-show');
+        sliderItems[j].classList.remove("slider-show");
+        sliderItems[j + 1].classList.add("slider-show");
 
-        radioButtons[j].classList.remove('radio-button-checked');
-        radioButtons[j + 1].classList.add('radio-button-checked');
+        radioButtons[j].classList.remove("radio-button-checked");
+        radioButtons[j + 1].classList.add("radio-button-checked");
 
         if (sliderItems[j + 1] === sliderItems[sliderItems.length - 1]) {
           forward.setAttribute("disabled", "disabled");
@@ -117,15 +117,15 @@ forward.addEventListener('click', function () {
     }
 });
 
-back.addEventListener('click', function () {
+back.addEventListener("click", function () {
   for (var j = 0; j < sliderItems.length; j++) {
-      if (sliderItems[j].classList.contains('slider-show')) {
+      if (sliderItems[j].classList.contains("slider-show")) {
         
-        sliderItems[j].classList.remove('slider-show');
-        sliderItems[j - 1].classList.add('slider-show');
+        sliderItems[j].classList.remove("slider-show");
+        sliderItems[j - 1].classList.add("slider-show");
 
-        radioButtons[j].classList.remove('radio-button-checked');
-        radioButtons[j - 1].classList.add('radio-button-checked');
+        radioButtons[j].classList.remove("radio-button-checked");
+        radioButtons[j - 1].classList.add("radio-button-checked");
 
         if (sliderItems[j] === sliderItems[sliderItems.length - 1]) {
           forward.removeAttribute("disabled");
